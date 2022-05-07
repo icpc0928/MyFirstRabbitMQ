36e326b599f9f5b9808430271249d1ef1e2b3514
LEOSPC\hikar <icpc0928@gmail.com>
2022-05-07 14:06:59 +0800

    20220507 隊列、消息 持久化

--- rabbitmq-hello/src/main/java/com/leo/rabbitmq/three/Task02.java
+++ rabbitmq-hello/src/main/java/com/leo/rabbitmq/three/Task02.java
@@ -2,6 +2,7 @@ package com.leo.rabbitmq.three;
 
 import com.leo.rabbitmq.utils.RabbitMqUtils;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
@@ -17,13 +18,18 @@ public class Task02 {
     public static void main(String[] args) throws Exception{
         Channel channel = RabbitMqUtils.getChannel();
 
+        //隊列聲明為持久化
+        //表示需要讓queue隊列進行持久化 萬一當機了重啟MQ依然有隊列記錄，但如果之前聲明的隊列不是持久化的,需要先把原先隊列刪除,或者重新創建一個持久化的隊列,不然會出現錯誤
+        boolean durable = true;
         //聲明隊列
-        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, durable, false, false, null);
         //從控制台中輸入信息
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()){
             String message = scanner.next();
-            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            //消息標記為持久化 BasicProperties => MessageProperties.PERSISTENT_TEXT_PLAIN  (消息要求保存到磁盤上)
+            //將消息標記為持久化並不能完全保證不會丟失消息，儘管他告訴rabbitMQ將消息保存到磁盤，但是這裡依然存在消息剛準備儲存在磁盤的時候,但是還沒有儲存完畢，消息還在緩存的一個間隔點。
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
             System.out.println("生產者發出消息: " + message);
 
         }
